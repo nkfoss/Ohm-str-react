24559b0d133fb1e65100bf814065a1e4289955ff
michael-small <small203@morris.umn.edu>
2021-02-26 12:52:28 -0600

    Add TODO to `CreateRecord FC` (functional component) as comments

--- src/components/Records/CreateRecord/CreateRecord.tsx
+++ src/components/Records/CreateRecord/CreateRecord.tsx
@@ -5,6 +5,28 @@ import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 import './CreateRecord.scss'
 
+// TODO: Epic #4: "Create the edit/add exercise page": https://github.com/nkfoss/Ohm-str-react/issues/4
+// EditExerciseRecord: also used for adding new exercises
+
+// 	--- TEMPLATE ----
+
+	// 	A button to cancel / go back
+	// 	A button to Delete the current exercise (if not in edit mode)
+
+	// 	Input for exercise name
+	// 	A dropdown menu to select the set-type (myo, mtor, rest-pause-double, etc.)
+	// 	Textarea for Notes
+
+	// 	Dynamic form (warm-up sets):
+	// 		A button to add a warmup-set element. 
+	// 		Each element contains a number, weight (input), reps (input), and button to delete element.
+
+	// 	Dynamic form (working sets):
+	// 		*** This will look different depending on the specific type of set. In general, it will have
+	// 			the set #, weight, and reps
+
+	// 	A button to submit the form
+
 export default function CreateRecord() {
 		const StyledTextField = styled(TextField)`
 			margin-bottom: 1rem;
