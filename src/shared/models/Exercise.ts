4128f7535361ec114d443c546ad62a732a124508
nathan <nathankfoss@gmail.com>
2021-03-02 16:59:02 -0600

    Update type definition for property of Exercise object
    Working sets could be many different types. We need to include them all

--- src/shared/models/Exercise.ts
+++ src/shared/models/Exercise.ts
@@ -1,11 +1,13 @@
 import { BasicSet } from "./BasicSet";
+import { DropSet } from "./DropSet";
+import { RestPauseSet } from "./RestPauseSet";
 
 export interface Exercise {
 	exerciseName: string,
 	setType: string,
 	exerciseNotes: string,
 	warmupSets: BasicSet[],
-	sets: BasicSet[]
+	workingSets: BasicSet[] | RestPauseSet | DropSet
 }
 
 // Note: The properties "warmupSets" "sets" could be any type that extends BasicSet[].
