988cb98015789cfbb4af9b3c740e238aec35bea8
nathan <nathankfoss@gmail.com>
2021-03-09 07:18:46 -0600

    Add Redux to top-level of App

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './store/reducers/workout'
+
+// Here's a line that is necessary for the redux browser extension.
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// Here we make the store available for our app, and the browser-extension debugger.
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
+// Don't forget to wrap the App with the 'Provider' element, and give the store as a prop!
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <Provider store={store}>        
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider> ,
+
   document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
