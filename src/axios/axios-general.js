2c1619204c6f6604242ad36f1b81ab26bf77a94a
nathan <nathankfoss@gmail.com>
2021-03-10 08:01:01 -0600

    Update axios instance

--- src/axios/axios-general.js
+++ src/axios/axios-general.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import PORT_NUM from '../../port'
 
 const instance = axios.create({
-	baseURL: process.env.NODE_ENV === "development" ? `http://localhost:${PORT_NUM}/` : "the innernet"
+	baseURL: process.env.NODE_ENV === "development" ? `http://localhost:3001/` : "the innernet"
 })
 
 export default instance;
